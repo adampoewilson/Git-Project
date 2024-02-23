da612b460deb6c42bfdd310344d74ce807d55685
adampoewilson <adam62385@gmail.com>
2024-02-22 23:17:13 -0500

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Adam Wilson on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git Project/Git_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_ProjectApp.swift
+//  Git Project
+//
+//  Created by Adam Wilson on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
